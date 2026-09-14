b8eb4d4ffde1869cafccbb3a535c6cfec473ff18
Sashok999 <sashok999709@gmail.com>
2026-09-14 09:40:58 +0300

    practice one end

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+def create_courses():
+    print("Enter course name:")
+    c = input()
+    return c
 
-def show_courses():
-    pass
+def show_employees(employees):
+    print(f"{len(employees)} employees")
+    for employee in employees:
+        print(employee)
 
-def show_employees():
-    pass
+def register_on_courses(employe, course):
+    course_reg[employe] = course
+    print(f"{employe} registered on course {course}")
 
-def register_on_courses():
-    pass
+
+
+course_reg = {}
+course = create_courses()
+employees = ["Ivan", "Peter", "Anton"]
+show_employees(employees)
+employee = register_on_courses(employees[1], course)
